7f102a9fea80c1071670b3c1187071c54edcc3ca
Carlos <tarea2git@gmail.com>
2025-04-15 13:14:29 +0200

    Estructura Proyecto y application.properties

---
+++ src/test/java/trixo/api/trixo_api/TrixoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package trixo.api.trixo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrixoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
