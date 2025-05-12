b3f576d425a4a1528eca3330efd940cb474de615
Carlos <tarea2git@gmail.com>
2025-05-12 20:39:29 +0200

    arreglos

--- src/main/java/trixo/api/trixo_api/controllers/UserController.java
+++ src/main/java/trixo/api/trixo_api/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import trixo.api.trixo_api.entities.User;
@@ -40,12 +41,9 @@ public class UserController {
     }
 
     @GetMapping("/getPreferences")
-    public ResponseEntity<String> getUserPreferences(@RequestBody String userID) {
-        if(userService.hasPreferences(userID)){
-            return ResponseEntity.ok("User preferences retrieved successfully");
-        } else {
-            return ResponseEntity.status(500).body("Error retrieving user preferences");
-        }
+    public ResponseEntity<Boolean> getUserPreferences(@RequestParam String userID) {
+        boolean hasPreferences = userService.hasPreferences(userID);
+        return ResponseEntity.ok(hasPreferences);
     }
 
     @PostMapping("/updatePreferences")
--- src/main/java/trixo/api/trixo_api/security/SecurityConfig.java
+++ src/main/java/trixo/api/trixo_api/security/SecurityConfig.java
@@ -16,8 +16,8 @@ public class SecurityConfig {
         http
             .authorizeHttpRequests(auth -> auth
                 //.requestMatchers(HttpMethod.GET, "/api/posts/**").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/posts/**").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/users/**").permitAll()
+                //.requestMatchers(HttpMethod.POST, "/api/posts/**").permitAll()
+                //.requestMatchers(HttpMethod.POST, "/api/users/**").permitAll()
                 .anyRequest().authenticated()
             )
             .oauth2ResourceServer(oauth2 -> oauth2
